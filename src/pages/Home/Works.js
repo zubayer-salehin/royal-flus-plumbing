851afdcf6f852beedcbb1e93297b6f0d5ac6c010
zubayer-salehin <freelancerzubear@gmail.com>
2022-10-08 00:26:19 +0600

    work section padding decrease

--- src/pages/Home/Works.js
+++ src/pages/Home/Works.js
@@ -5,8 +5,8 @@ import { faCartShopping, faThumbsUp, faTruckArrowRight } from '@fortawesome/free
 
 const Works = () => {
     return (
-        <div className='customContainer pb-[84px]'>
-            <h1 className='text-4xl text-gray-900 font-bold pt-[64px] pb-[70px] text-center'>Here's How We Operate</h1>
+        <div className='customContainer pb-[24px] sm:pb-[84px]'>
+            <h1 className='text-4xl text-gray-900 font-bold pb-[70px] text-center'>Here's How We Operate</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
                 <div>
                     <FontAwesomeIcon className='text-5xl text-green-500 mb-3' icon={faCartShopping} />
